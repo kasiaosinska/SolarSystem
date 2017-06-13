0fec99a5631e67f7ae540db84e1b9e348d823579
Kasia Osinska <katarzynabosinska@gmail.com>
2017-06-14 00:12:12 +0200

    Create new components in Home site - skills

---
+++ dev/components/skills.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Skills extends React.Component {
+    render() {
+
+        return (
+            <div className="container-fluid sky">
+                <div className="rocket">
+                    <div className="rocket-fire">
+                        <div className="fire yellow-fire">
+                            <div className="fire orange-fire">
+                                <div className="fire red-fire"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="cloud s1"><p className="skills">React.JS</p></div>
+                <div className="cloud s2"><p className="skills">HTML5</p></div>
+                <div className="cloud s3"><p className="skills">CSS3</p></div>
+                <div className="cloud s4"><p className="skills">SASS</p></div>
+                <div className="cloud s5"><p className="skills">Webpack</p></div>
+                <div className="cloud s6"><p className="skills">JavaScript</p></div>
+                <div className="cloud s7"><p className="skills">jQuery</p></div>
+                <div className="cloud s8"><p className="skills">ECMASCRIPT 6</p></div>
+                <div className="cloud s9"><p className="skills">RWD</p></div>
+                <div className="cloud s10"><p className="skills">Git</p></div>
+            </div>
+        )
+    }
+}
+
+export default Skills
